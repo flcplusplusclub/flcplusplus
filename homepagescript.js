20ce9147bdcbf65d9121c424e960b33b0f68fa26
Katz100 <cody.hopkins108@gmail.com>
2024-10-27 13:11:44 -0700

    change img to background-img

--- homepagescript.js
+++ homepagescript.js
@@ -4,16 +4,15 @@ const ads = [
     "ads/ad3.PNG",
     "ads/ad4.jpg",
     "ads/ad5.PNG"
-]
+];
 
 window.onload = function() {
-    ad = document.getElementById("ads")
+    ad = document.querySelector(".item4");
     let current = 0;
 
     function displayNewAd() {
-        ad.src = ads[current];
-        current = (current + 1) % ads.length
+        ad.style.backgroundImage =  `url(${ads[current]})`;
+        current = (current + 1) % ads.length;
     }
-
     setInterval(displayNewAd, 3000);
 }
